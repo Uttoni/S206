5bc33141358a2642a6c6d44086a644b223181f11
Uttoni <uttoni.brandani@ges.inatel.br>
2021-11-05 23:52:29 -0300

    Primeiro teste com o cypress

---
+++ testes_inatel/cypress/integration/aulaInatel/globalqa.spec.js
@@ -0,0 +1,6 @@
+/// <reference types="cypress"/>
+
+describe['Suíte de testes', ()=>{
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login');
+    cy.get('h1');
+}]
